32e14536f63ca5257cf2b2c3a7f8749161ef460b
ajvadntr <91082660+ajvadntr@users.noreply.github.com>
2022-02-03 21:09:58 +0530

    Update custom_thumb.py

--- root/plugins/custom_thumb.py
+++ root/plugins/custom_thumb.py
@@ -27,7 +27,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 @Client.on_message(filters.photo)
 async def save_photo(c,m):
-    v = await m.reply_text("Saving Thumbnail",True)
+    v = await m.reply_text("Sᴀᴠɪɴɢ Tʜᴜᴍʙɴᴀɪʟ",True)
     if m.media_group_id is not None:
         # album is sent
         download_location = Config.DOWNLOAD_LOCATION + "/thumb/" + str(m.from_user.id) + "/" + str(m.media_group_id) + "/"
